480b0e935642482383e802bf63e2220d7d88325c
Franklin Bristow <fbristow@gmail.com>
2014-11-22 09:55:32 -0600

    Add install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PWD=$(pwd)
+
+mkdir -p ~/.config/fish
+
+ln -s $PWD/.config/fish/config.fish ~/.config/fish/config.fish
+ln -s $PWD/.tmux.conf ~/.tmux.conf
+ln -s $PWD/.oh-my-fish ~/.oh-my-fish
