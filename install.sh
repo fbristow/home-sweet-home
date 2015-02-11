e4e66c26dd77207e62b8085d53b166cc255d583f
Franklin Bristow <fbristow@gmail.com>
2015-02-11 16:25:00 -0600

    Add a default vimrc.

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ mkdir -p ~/.config/fish
 ln -s $PWD/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.oh-my-fish ~/.oh-my-fish
+ln -s $PWD/.vimrc ~/.vimrc
