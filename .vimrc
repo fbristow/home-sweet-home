47e900d45e58eaf51d38338003be7efc024d7e41
Franklin Bristow <fbristow@gmail.com>
2017-09-09 14:52:08 -0500

    Add some nice defaults for vim, with some nice plugins.

--- .vimrc
+++ .vimrc
@@ -1,1 +1,15 @@
+execute pathogen#infect()
+
+filetype plugin indent on
 syntax on
+
+" config table mode so it plays nicely with pandoc
+
+let g:table_mode_corner_corner='+'
+let g:table_mode_align_char=':'
+let g:table_mode_header_fillchar='='
+
+" quit beeping! flash at me if you have a problem
+
+set visualbell
+set noerrorbells
--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 PWD=$(pwd)
 
-ln -s $PWD/.tmux.conf ~/.tmux.conf
-ln -s $PWD/.vimrc ~/.vimrc
+# tmux
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+
+# vim
+ln -sf $PWD/.vimrc ~/.vimrc
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+pushd ~/.vim
+git clone https://github.com/dhruvasagar/vim-table-mode bundle/table-mode
+git clone https://github.com/tpope/vim-sensible bundle/vim-sensible
+popd
 
 # install oh my fish:
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
