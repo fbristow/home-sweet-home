43ede36437d0f5075c48dcb9ab786fe58d4c8dbf
Franklin Bristow <fbristow@gmail.com>
2019-07-01 10:04:53 -0500

    Use a specific revision of table-mode.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 pushd ~/.vim
-git clone https://github.com/dhruvasagar/vim-table-mode bundle/table-mode
+git clone --branch v4.6.7 https://github.com/dhruvasagar/vim-table-mode bundle/table-mode
 git clone https://github.com/tpope/vim-sensible bundle/vim-sensible
 popd
 
