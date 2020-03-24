f29f4095a4ab81cba42e7d537e5b3bbc89f3125a
Franklin Bristow <fbristow@gmail.com>
2020-03-24 13:41:09 -0500

    Vimwiki uses tags, not branches.

--- install.sh
+++ install.sh
@@ -14,7 +14,10 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 pushd ~/.vim
 git clone --branch v4.6.7 https://github.com/dhruvasagar/vim-table-mode bundle/table-mode
 git clone https://github.com/tpope/vim-sensible bundle/vim-sensible
-git clone --branch 2.4.1 https://github.com/vimwiki/vimwiki bundle/vimwiki
+git clone https://github.com/vimwiki/vimwiki bundle/vimwiki
+pushd bundle/vimwiki
+git checkout v2.4.1
+popd
 popd
 
 # install oh my fish:
