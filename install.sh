fa51efcda5bd210e446f678f137da0687231726f
Franklin Bristow <fbristow@gmail.com>
2022-08-12 16:10:09 -0500

    Turn off vimwiki.
    It's honestly turning into more of a bother than a help, I keep
    getting burned when I'm trying to do something in plain markdown.

--- install.sh
+++ install.sh
@@ -17,7 +17,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 pushd ~/.vim
 git clone --branch v4.6.7 https://github.com/dhruvasagar/vim-table-mode bundle/table-mode
 git clone https://github.com/tpope/vim-sensible bundle/vim-sensible
-git clone https://github.com/vimwiki/vimwiki bundle/vimwiki
+#git clone https://github.com/vimwiki/vimwiki bundle/vimwiki
+git clone https://github.com/dag/vim-fish
 pushd bundle/vimwiki
 git checkout v2.4.1
 popd
